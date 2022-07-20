552d9eb0e315c712f730094584abe4351468dda2
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-07-20 23:19:32 +0200

    chapter_5.rs

--- src/chapter_5.rs
+++ src/chapter_5.rs
@@ -5,14 +5,37 @@ struct User {
     sign_in_count: u64,
 }
 
-pub ( crate ) fn chapter_5(){
+pub(crate) fn chapter_5() {
     let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
         active: true,
         sign_in_count: 1,
     };
+    let user2 = User {
+        email: String::from("another@passt.com"),
+        ..user1
+    };
     user1.email = String::from("anotheremail@example.com");
-let email = user1.email;
-    println!("{email}")
+    let email2 = user2.email;
+    let email = user1.email;
+    println!("{email}");
+    println!("{email2}");
 }
+
+pub(crate) fn area() {
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+
+
+    fn area(width: u32, height: u32) -> u32 {
+        width * height
+    }
+}
+
+
